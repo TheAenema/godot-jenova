ebfe5adc3ebc4ce95588c25a4eabd9e87808f92a
TheAenema <thegreatnone1995@gmail.com>
2025-08-21 03:21:56 +0330

    Version Data Update

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
-short_name = "godot"
-name = "Godot Engine"
+short_name = "godot-jnvc"
+name = "Godot Engine Jenova Compatible"
 major = 4
 minor = 5
 patch = 0
 status = "beta"
 module_config = ""
-website = "https://godotengine.org"
+website = "https://jenova-framework.github.io"
 docs = "latest"
